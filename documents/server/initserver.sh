b54503bf4fed7b5c937666cdad395a529079d5bc
Cheng Zhang <850576570@qq.com>
2020-08-13 17:15:00 +0800

    添加 make 的 sudo 权限

--- documents/server/initserver.sh
+++ documents/server/initserver.sh
@@ -78,8 +78,9 @@ sysctl -p
 #修改用户组 sudoer 权限
 #https://segmentfault.com/a/1190000007394449
 touch /etc/sudoers.d/members
-echo "Cmnd_Alias ALLOW_APT_CMD=/usr/bin/apt,/usr/bin/apt-get,!/usr/bin/apt remove,!/usr/bin/apt autoremove,!/usr/bin/apt-get remove,!/usr/bin/apt-get purge,!/usr/bin/apt-get autoremove" >> /etc/sudoers.d/members
-echo "%members ALL=ALLOW_APT_CMD" >> /etc/sudoers.d/members
+echo "Cmnd_Alias ALLOW_APT_CMD=/usr/bin/apt,/usr/bin/apt-get,!/usr/bin/apt remove,!/usr/bin/apt autoremove,!/usr/bin/apt-get remove,!/usr/bin/apt-get purge,!/usr/bin/apt-get autoremove" > /etc/sudoers.d/members
+echo "Cmnd_Alias ALLOW_MAKE_CMD=/usr/bin/make" >> /etc/sudoers.d/members
+echo "%members ALL=ALLOW_APT_CMD,ALLOW_MAKE_CMD" >> /etc/sudoers.d/members
 
 
 #安装 ldap 依赖库，配置 ldap 服务
--- documents/server/patchserver.sh
+++ documents/server/patchserver.sh
@@ -1,7 +1,5 @@
 #以 sudo 运行该脚本
 
-#https://linux.die.net/man/5/cachefilesd.conf
-sed -i "s/10%/30%/g" /etc/cachefilesd.conf
-sed -i "s/7%/25%/g" /etc/cachefilesd.conf
-sed -i "s/3%/20%/g" /etc/cachefilesd.conf
-systemctl restart cachefilesd
+echo "Cmnd_Alias ALLOW_APT_CMD=/usr/bin/apt,/usr/bin/apt-get,!/usr/bin/apt remove,!/usr/bin/apt autoremove,!/usr/bin/apt-get remove,!/usr/bin/apt-get purge,!/usr/bin/apt-get autoremove" > /etc/sudoers.d/members
+echo "Cmnd_Alias ALLOW_MAKE_CMD=/usr/bin/make" >> /etc/sudoers.d/members
+echo "%members ALL=ALLOW_APT_CMD,ALLOW_MAKE_CMD" >> /etc/sudoers.d/members
